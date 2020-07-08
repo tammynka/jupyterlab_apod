3a6845637c8e21ec5f45f172071d1b2c6f7d5dd7
Tammy Kaldes <tammyk@ziprecruiter.com>
2020-07-08 15:39:14 +0300

    first commit, working jupyterlab extension

---
+++ src/index.ts
@@ -0,0 +1,150 @@
+import {
+  ILayoutRestorer,
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+import {
+  ICommandPalette,
+  MainAreaWidget,
+  WidgetTracker
+} from '@jupyterlab/apputils';
+import { Message } from '@lumino/messaging';
+import { Widget } from '@lumino/widgets';
+
+interface IAPODResponse {
+  copyright: string;
+  date: string;
+  explanation: string;
+  media_type: 'video' | 'image';
+  title: string;
+  url: string;
+}
+
+class APODWidget extends Widget {
+  /**
+   * Construct a new APOD Widget
+   */
+  constructor() {
+    super();
+    this.addClass('my-apodWidget');
+
+    // Add an image element to the panel
+    this.img = document.createElement('img');
+    this.node.appendChild(this.img);
+
+    // Add a summary element to the panel
+    this.summary = document.createElement('p');
+    this.node.appendChild(this.summary);
+  }
+
+  // The image element associated with the widget
+  readonly img: HTMLImageElement;
+
+  // The summary element associated with the widget
+  readonly summary: HTMLParagraphElement;
+
+  /**
+   * Handle update requests for the widget
+   * @param msg
+   */
+  async onUpdateRequest(msg: Message): Promise<void> {
+    // Fetch info about a random picture
+    const response = await fetch(
+      `https://api.nasa.gov/planetary/apod?api_key=BUYtExVKPICEucmqtsTrC0FJ0mWDqA44o8zbFlZn&date=${this.randomDate()}`
+    );
+    console.log('response', response);
+    if (!response.ok) {
+      const data = await response.json();
+      if (data.error) {
+        this.summary.innerText = data.error.message;
+      } else {
+        this.summary.innerText = response.statusText;
+      }
+      return;
+    }
+
+    const data = (await response.json()) as IAPODResponse;
+    if (data.media_type === 'image') {
+      // Populate the image
+      this.img.src = data.url;
+      this.img.title = data.title;
+      this.summary.innerText = data.title;
+      if (data.copyright) {
+        this.summary.innerText += ` (Copyright ${data.copyright})`;
+      }
+    } else {
+      this.summary.innerText = 'Random APOD was not a picture';
+    }
+  }
+
+  /**
+   * Get a random date string in YYYY-MM-DD format
+   */
+  randomDate(): string {
+    const start = new Date(2010, 1, 1);
+    const end = new Date();
+    const randomDate = new Date(
+      start.getTime() + Math.random() * (end.getTime() - start.getTime())
+    );
+    return randomDate.toISOString().slice(0, 10);
+  }
+}
+
+function activate(
+  app: JupyterFrontEnd,
+  palette: ICommandPalette,
+  restorer: ILayoutRestorer
+): void {
+  let widget: MainAreaWidget<APODWidget>;
+
+  // Add an application command
+  const command = 'apod:open';
+  app.commands.addCommand(command, {
+    label: 'Random Astronomy Picture',
+    execute: () => {
+      if (!widget) {
+        // Create a new widget if one does not exist
+        const content = new APODWidget();
+        widget = new MainAreaWidget({ content });
+        widget.id = 'apod-jupyterlab';
+        widget.title.label = 'Astronomy Picture';
+        widget.title.closable = true;
+      }
+      if (!tracker.has(widget)) {
+        tracker.add(widget);
+      }
+      if (!widget.isAttached) {
+        // Attach the widget to the main work area if it's not there
+        app.shell.add(widget, 'main');
+      }
+      widget.content.update();
+
+      // Activate the widget
+      app.shell.activateById(widget.id);
+    }
+  });
+
+  // Add the command to the palette.
+  palette.addItem({ command, category: 'AAA Tutorial' });
+
+  // Track and restore the widget state
+  const tracker = new WidgetTracker<MainAreaWidget<APODWidget>>({
+    namespace: 'apod'
+  });
+  restorer.restore(tracker, {
+    command,
+    name: () => 'apod'
+  });
+}
+
+/**
+ * Initialization data for the jupyterlab_apod extension.
+ */
+const extension: JupyterFrontEndPlugin<void> = {
+  id: 'jupyterlab_apod',
+  autoStart: true,
+  requires: [ICommandPalette, ILayoutRestorer],
+  activate
+};
+
+export default extension;
